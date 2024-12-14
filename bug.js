5ff77bccd0ffd620360e17f54b8d2663b91c2b1e
BugHunter-X <askzenai@gmail.com>
2024-12-14 11:22:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,10 @@
+```javascript
+<div class="flex">
+  <div class="bg-red-500 p-4">
+    <!-- Some content -->
+  </div>
+  <div class="bg-blue-500 p-4">
+    <!-- Some content -->
+  </div>
+</div>
+```
